0dc5327e6e93e33425d30482dc65a324570003b7
chelcie de almeida <chelciedealmeida@chelcies-MBP.lan>
2021-05-07 04:10:17 -0700

    gen htmll

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 //listen for heroku port
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
